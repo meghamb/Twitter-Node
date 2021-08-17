3415652ee8a4a3d05dd2b15a82da166f33847b4f
M B, Megha <megha.mb@philips.com>
2021-08-17 19:50:48 +0530

    added express-ejs-layouts

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = require('./src/routes/index');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 
-app.use('/', router);
+app.use(expressLayouts);
+app.set('layout', './layouts/layout');
+
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
+app.use('/', router);
 
 app.listen(3000, () => {
     console.log('Server started at port 3000');
